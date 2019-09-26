62d2e52bc59b26769ca3dd43fde94eb00d617327
Nick Guerra <nickg@maliciousgames.com>
2019-09-25 19:51:36 -0500

    basic structure added

--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
+import jquery from 'jquery';
+require('bootstrap');
+require('jquery.easing');
 
-Vue.config.productionTip = false
+require('bootstrap/dist/css/bootstrap.css');
+require('devicons/css/devicons.css');
+require('font-awesome/css/font-awesome.css');
+
+Vue.prototype.jquery = jquery;
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  render: (h) => h(App)
+});
