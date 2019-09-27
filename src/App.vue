fc67fcce048ebcd35a332996400ac67add54dfd0
Nick Guerra <nickg@maliciousgames.com>
2019-09-26 20:00:15 -0500

    added data

--- src/App.vue
+++ src/App.vue
@@ -119,24 +119,21 @@
 
           <div class="resume-item d-flex flex-column flex-md-row mb-5">
             <div class="resume-content mr-auto">
-              <h3 class="mb-0">University of Colorado Boulder</h3>
-              <div class="subheading mb-3">Bachelor of Science</div>
-              <div>Computer Science - Web Development Track</div>
-              <p>GPA: 3.23</p>
+              <h3 class="mb-0">University of New Orleans</h3>
+              <div class="subheading mb-3">Bachelor of Arts</div>
+              <div>Philosophy</div>
+              <p>2002</p>
             </div>
-            <div class="resume-date text-md-right">
-              <span class="text-primary">August 2006 - May 2010</span>
-            </div>
+            <div class="resume-date text-md-right"></div>
           </div>
 
           <div class="resume-item d-flex flex-column flex-md-row">
             <div class="resume-content mr-auto">
-              <h3 class="mb-0">James Buchanan High School</h3>
-              <div class="subheading mb-3">Technology Magnet Program</div>
-              <p>GPA: 3.56</p>
+              <h3 class="mb-0">Brother Martin High School</h3>
+              <div class="subheading mb-3">New Orleans, LA</div>
             </div>
             <div class="resume-date text-md-right">
-              <span class="text-primary">August 2002 - May 2006</span>
+              <span class="text-primary">1993 - 1997</span>
             </div>
           </div>
         </div>
@@ -148,6 +145,7 @@
 
           <div class="subheading mb-3">Programming Languages &amp; Tools</div>
           <ul class="list-inline list-icons">
+            <!--TODO Add icons for C#, .Net etc -->
             <li class="list-inline-item">
               <i class="devicons devicons-html5"></i>
             </li>
@@ -163,21 +161,14 @@
             <li class="list-inline-item">
               <i class="devicons devicons-sass"></i>
             </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-less"></i>
-            </li>
+
             <li class="list-inline-item">
               <i class="devicons devicons-bootstrap"></i>
             </li>
             <li class="list-inline-item">
               <i class="devicons devicons-wordpress"></i>
             </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-grunt"></i>
-            </li>
-            <li class="list-inline-item">
-              <i class="devicons devicons-gulp"></i>
-            </li>
+
             <li class="list-inline-item">
               <i class="devicons devicons-npm"></i>
             </li>
@@ -188,10 +179,6 @@
             <li>
               <i class="fa-li fa fa-check"></i>
               Mobile-First, Responsive Design
-            </li>
-            <li>
-              <i class="fa-li fa fa-check"></i>
-              Cross Browser Testing &amp; Debugging
             </li>
             <li>
               <i class="fa-li fa fa-check"></i>
@@ -208,57 +195,8 @@
       <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
         <div class="my-auto">
           <h2 class="mb-5">Interests</h2>
-          <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skiier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-          <p
-            class="mb-0"
-          >When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technolgy advancements in the front-end web development world.</p>
-        </div>
-      </section>
-
-      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
-        <div class="my-auto">
-          <h2 class="mb-5">Awards &amp; Certifications</h2>
-          <ul class="fa-ul mb-0">
-            <li>
-              <i class="fa-li fa fa-trophy text-warning"></i>
-              Google Analytics Certified Developer
-            </li>
-            <li>
-              <i class="fa-li fa fa-trophy text-warning"></i>
-              Mobile Web Specialist - Google Certification
-            </li>
-            <li>
-              <i class="fa-li fa fa-trophy text-warning"></i>
-              1
-              <sup>st</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition 2009
-            </li>
-            <li>
-              <i class="fa-li fa fa-trophy text-warning"></i>
-              1
-              <sup>st</sup>
-              Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
-            </li>
-            <li>
-              <i class="fa-li fa fa-trophy text-warning"></i>
-              2
-              <sup>nd</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition 2008
-            </li>
-            <li></li>
-            <li>
-              <i class="fa-li fa fa-trophy text-warning"></i>
-              1
-              <sup>st</sup>
-              Place - James Buchanan High School - Hackathon 2006
-            </li>
-            <li>
-              <i class="fa-li fa fa-trophy text-warning"></i>
-              3
-              <sup>rd</sup>
-              Place - James Buchanan High School - Hackathon 2005
-            </li>
-          </ul>
+          <p>I enjoy exercise and the outdoors, as well as pc strategy games in my spare time.</p>
+          <p class="mb-0"></p>
         </div>
       </section>
     </div>
